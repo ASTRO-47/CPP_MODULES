e5d60aaee5f57923e763d0044ba43056a2aca7c5
Imad Ez-Zaghba <iez-zagh@e2r3p14.1337.ma>
2024-11-21 16:10:52 +0100

    ex_00 done

---
+++ cpp_00/ex_00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		int i = 1;
+		while (i < ac)
+		{
+			int j = 0;
+			while (av[i][j])
+				std::cout << (char)toupper(av[i][j++]);
+			if (i < ac - 1)
+				std::cout << ' ';
+			i++;
+		}
+	}
+	std::cout << "\n";
+	return (0);
+}
