56125f788943ca00ab33c9ad68e7bcfbe47b9ab0
aspro-47 <ezzaghba38@gmail.com>
2024-12-02 18:11:08 +0100

    adding the insetion operator

--- cpp_02/ex01/Fixed.cpp
+++ cpp_02/ex01/Fixed.cpp
@@ -1,23 +1,34 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(const float)
+Fixed::Fixed(const int n) : fixed_point(n)
 {
+    std::cout << "Int constructor called" << std::endl;
+    
 }
-Fixed::Fixed()
+
+Fixed::Fixed(const float n) : fixed_point(roundf(n * (1 << Fixed::fr_bits)))
 {
+    std::cout << "Float constructor called" << std::endl;
+
+}
+
+
+Fixed::Fixed() : fixed_point(0)
+{
+    std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
+    std::cout << "Destructor called" <<std::endl;
     // no dynamic allocation to delete
 }
 
 Fixed::Fixed( const Fixed  &obj)
 {
-    if (obj.fixed_point)
-        std::cout << "" << std::flush;
+    std::cout << "Copy constructor called" << std::endl;
+    this->fixed_point = obj.fixed_point;
 }
-
 
 
 Fixed& Fixed::operator=(const Fixed &obj)
@@ -25,6 +36,12 @@ Fixed& Fixed::operator=(const Fixed &obj)
     std::cout << "Copy assignment operator called" << std::endl;
     if (this == &obj)
         return *this;
-    
+    this->fixed_point = obj.fixed_point;;
     return *this;
+}
+
+std::ostream &operator<<(std::ostream &out, Fixed &i)
+{
+    out << i.to
+    return out;
 }
--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -9,13 +9,16 @@ private:
     int fixed_point;
     static const int fr_bits;
 public:
-    Fixed(const float);
+    Fixed(const int n);
+    Fixed(const float n);
     Fixed();
     ~Fixed();
     Fixed(const Fixed &obj);
     Fixed& operator=(const Fixed &obj);
-    float toFloat( void ) const;
-    int toInt( void ) const;
+    // float toFloat( void ) const;
+    // int toInt( void ) const;
 };
+
+std::ostream &operator<<(std::ostream &out, Fixed &i);
 
 #endif
