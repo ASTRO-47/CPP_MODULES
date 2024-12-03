6708c36a0b879c04177d84a8f8f3fcb369b745fa
aspro-47 <ezzaghba38@gmail.com>
2024-12-03 16:08:16 +0100

    statrting on ex02

---
+++ cpp_02/ex02/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+private:
+    int fixed_point_value;
+    static const int fr_bits;
+public:
+    Fixed(const int n);
+    Fixed(const float n);
+    Fixed();
+    ~Fixed();
+    Fixed(const Fixed &obj);
+    Fixed& operator=(const Fixed &obj);
+    float toFloat( void ) const;
+    int toInt( void ) const;
+};
+
+std::ostream& operator<<(std::ostream &out, Fixed const &i);
+
+#endif
