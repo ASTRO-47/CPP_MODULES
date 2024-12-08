62e608e3ce893254547b92b4c4ad9b5d64ee8c49
aspro-47 <ezzaghba38@gmail.com>
2024-12-08 16:50:32 +0100

    starting in cpp_03

---
+++ cpp_03/ex00/ClapTrap.cpp
@@ -0,0 +1,33 @@
+#include "ClapTrap.hpp"
+ 
+ClapTrap::ClapTrap(std::string name_) : name(name_), h_point(10), e_points(10), at_damage(0)
+{
+    std::cout << "constructor called" << std::endl;
+}
+
+
+ClapTrap::ClapTrap(void)
+{
+   std::cout << "default constructor called" << std::endl;
+
+}
+
+ClapTrap::~ClapTrap(void)
+{
+    std::cout << "destructor called" << std::endl;
+}
+
+void    ClapTrap::attack(const std::string &target)
+{
+    std::cout << ClapTrap <<  "attacks" << target <<  "causing" <<  <damage>  << "points of damage!" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream &out, const ClapTrap &other)
+{
+    out << other.get_name();
+    return out;
+}
+
+const std::string& ClapTrap::get_name(){
+    return (this->name);
+}
---
+++ cpp_03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap{
+private:
+    const std::string name;  //is it should be const or not think about it
+    unsigned int h_point;
+    unsigned int e_points;
+    unsigned int at_damage;
+public:
+    ClapTrap(void);
+    ClapTrap(std::string name_);
+    ~ClapTrap();
+
+    const  std::string& get_name();
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+std::ostream& operator<<(std::ostream &out, const ClapTrap &ohter);
+
+#endif
