990e9d393bd2db02565c1867f3f0069a9071f78f
Imad Ez-Zaghba <iez-zagh@e2r1p3.1337.ma>
2024-12-09 11:59:04 +0100

    ex00 in cpp_03 done

---
+++ cpp_03/ex01/ClapTrap.cpp
@@ -0,0 +1,72 @@
+#include "ClapTrap.hpp"
+ 
+ClapTrap::ClapTrap(std::string name_) : name(name_), h_point(10), e_points(10), at_damage(0)
+{
+    std::cout << "default constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+    std::cout << "destructor called" << std::endl;
+}
+
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+    this->name = other.name;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &other)
+{
+    if (this == &other)
+        return *this;
+    this->h_point = other.h_point; // if we use just var names without this what should happen
+    this->at_damage = other.at_damage;
+    this->e_points = other.e_points;
+    this->h_point = other.h_point;
+    return *this;
+}
+
+void    ClapTrap::attack(const std::string &target)
+{
+    if (!this->h_point)
+    {
+        std::cout << "ClapTrap "<< this->name << " is dead" << std::endl;
+        return ;
+    }
+    if (!this->e_points)
+    {
+        std::cout << "ClapTrap " << this->name << " have no enough energy points to attack !!" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap " << this->name <<  " attacks " << target <<  " causing " << this->at_damage << " points of damage!" << std::endl;
+    this->e_points--;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if (this->h_point <= amount)
+    {
+        std::cout << "ClapTrap "<< this->name << " is dead" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap "<< this->name <<  " takes "  << amount  << " of damage!" << std::endl;
+    this->h_point -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (this->h_point <= 0) //nothing can do if he is already dead
+    {
+        std::cout << this->name << " is dead" << std::endl;
+        return ;
+    }
+    if (!this->e_points)
+    {
+        std::cout << "oooh " << " no enough enrgy points" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap " << this->name << " repairs itself for " << amount << " hit points!" << std::endl;
+    this->h_point += amount;
+    this->e_points--;
+}
---
+++ cpp_03/ex01/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap{
+private:
+    std::string name;  //is it should be const or not think about it
+    unsigned int h_point;
+    unsigned int e_points;
+    unsigned int at_damage;
+public:
+    ClapTrap(std::string name_);
+    ClapTrap(const ClapTrap &other);
+    ClapTrap &operator=(const ClapTrap &other);
+    ~ClapTrap();
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+#endif
