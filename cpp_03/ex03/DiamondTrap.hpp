1cb23bc8a68a022a29524736737736d89bf4633d
aspro-47 <ezzaghba38@gmail.com>
2024-12-11 10:24:03 +0100

    working on ex03

---
+++ cpp_03/ex03/DiamondTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "ScavTrap.hpp"
+
+
+class test{
+private:
+    int pr;
+public:
+    int pub;
+};
+
+
+class DiamondTrap : public test
+{
+private:
+    //
+public:
+    
+};
+
+#endif
