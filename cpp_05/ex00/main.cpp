cdddb88c0eb86cd7a03f0f4d9949283b92eaaf1f
iez-zagh <ezzaghba38@gmail.com>
2025-01-07 15:16:20 +0100

    working on execptions

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-
-int main()
+int main ()
 {
-    puts("hello");
+    std::string name = "word";
+    
+    try
+    {
+        // std::cout << name.at(4) << std::endl;
+        int *arr = new int[99999999999999];
+    }
+    catch (...)
+    {
+        std::cout << "out of range exption thronw\n";
+    }
+    // catch (std::bad_alloc &r)
+    // {
+    //     std::cout << "bad alloc execption thrown\n";
+    // }
+
 }
