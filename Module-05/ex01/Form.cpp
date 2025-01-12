add1fc1b223b1e194e3473df5b63f2da8036f8da
aspro-47 <ezzaghba38@gmail.com>
2025-01-12 13:32:54 +0100

    working on ex01

---
+++ Module-05/ex01/Form.cpp
@@ -0,0 +1,71 @@
+#include "Form.hpp"
+
+Form::Form(std::string name_, int to_sign, int to_excute) : name(name_), required_grade_2_excute(to_excute), required_grade_2_sign(to_sign),signed_(false)
+{
+    if (required_grade_2_excute < 1 || required_grade_2_sign < 1)
+        throw GradeTooHighException();
+    if (required_grade_2_excute > 150 || required_grade_2_excute > 150)
+        throw GradeTooLowException();
+    std::cout << "new Form for "<< name << " is created\n";
+}
+
+Form::Form(const Form & other) : name(other.name), required_grade_2_excute(other.required_grade_2_excute), required_grade_2_sign(other.required_grade_2_sign),signed_(false)
+{
+
+}
+
+Form &Form::operator=(const Form & other)
+{
+    if (this != &other)
+    {
+        signed_ = other.signed_;
+    }
+    return *this;
+}
+
+std::string Form::getName() const
+{
+    return name;
+}
+
+bool Form::get_status() const
+{
+    return signed_;
+}
+
+int Form::get_r_sign() const
+{
+    return required_grade_2_sign;
+}
+
+int Form::get_r_excute() const
+{
+    return required_grade_2_excute;
+}
+
+void Form::beSigned(const Bureaucrat &bur)
+{
+    if (bur.getGrade() <= required_grade_2_sign)
+    {
+        std::cout << "Bureaucrat " << bur.getName() << " signed the Form\n";
+        signed_  = true;
+    }
+    else 
+        throw GradeTooLowException();
+}
+
+
+
+
+const char * Form::GradeTooHighException::what() const throw()
+{
+    return ("Too high grade");
+}
+
+const char * Form::GradeTooLowException::what() const throw()
+{
+    return ("Too low grade");
+}
+
+
+Form::~Form() {}
--- Module-05/ex01/Form.hpp
+++ Module-05/ex01/Form.hpp
@@ -1,14 +1,42 @@
 #pragma once
 
-
-#include <iostream>
+#include "Bureaucrat.hpp"
 
 class Form
 {
 private:
-
-    // bool signed;
-     int grade;
+    bool signed_;
+    const std::string name;
+    const int required_grade_2_sign;
+    const int required_grade_2_excute;
+    Form();
 public:
-    // Form(){};
+    Form(std::string name_, int to_sign, int to_excute);
+    Form &operator=(const Form &other);
+    Form(const Form &other);
+
+    std::string getName() const;
+    bool get_status() const;
+    int get_r_sign() const;
+    int get_r_excute() const;
+
+    void beSigned(const Bureaucrat &bur);
+
+    class GradeTooHighException: public std::exception 
+    {
+    public:
+        const char * what() const throw();
+    } ;
+
+    class GradeTooLowException: public std::exception 
+    {
+    public:
+        const char * what() const throw();
+    } ;
+
+
+
+    ~Form();
 } ; 
+
+std::ostream& operator<<(std::ostream &out, const Form &f);
