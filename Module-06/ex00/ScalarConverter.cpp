46965fad194dfbb6050a2a24dbfb33e2ff0f791f
aspro-47 <ezzaghba38@gmail.com>
2025-01-16 00:12:30 +0100

    adding the type casting

---
+++ Module-06/ex00/ScalarConverter.cpp
@@ -0,0 +1,14 @@
+#include "ScalarConverter.hpp"
+
+// for the canonical form
+
+ScalarConverter::ScalarConverter() {}
+ScalarConverter::ScalarConverter(const ScalarConverter &other) {
+    (void) other;
+}
+ScalarConverter & ScalarConverter::operator=(const ScalarConverter &other) { 
+    (void) other;
+    return *this;
+}
+ScalarConverter::~ScalarConverter() {}
+
--- Module-06/ex00/main.cpp
+++ Module-06/ex00/main.cpp
@@ -1,39 +1,35 @@
-#include "Bureaucrat.hpp"
+
+#include <iostream>
+
+// operator overloading and function overloading are a compile time polymorphism
+
+// the virtual methods and function overiding are the compile time polymorphism
+
+// there are 4 types of casting in c++: static_cast dynamic_cast reinterpret_cast  const_cast
+
+// const_cast used for refs , pointers or data-members
+
+// those are operators in c++
+// auto promotion (implicit casting)
 
 
-// stack unwding
 
-// when catch a throwen exception by value could made a slicing
-
-// another reason to catch excptio by reference make you catch every class derived from the std::exception class
-// and this because of the polymorphism
-
-// in a try block if an exception throwed all the work inside the block canceled (destroyed)
-// the  exception objects stored in special memory region that's its lifetime more than the usual objects
-
-
-// if there is no catch block and an exception throwen a function called unxpected and stops the code
-
+using namespace std;
 
 int main ()
 {
-    // two example (consturcting the object, modify the grade)
-    try
-    {
-        Bureaucrat imad("imad", 10);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << "failed: " << e.what() << '\n';
-    }
+    float f = 1.1;
+    int i = f;
+
+    // cout << (int)hello;
+    // cout << static_cast<char *> (i) ;
+
     
-    Bureaucrat imad("imad_beta", 1);
-    try
-    {
-        imad.upgrade();
-    }
-    catch(Bureaucrat::GradeTooHighException &e)
-    {
-        std::cerr << "faild: " << e.what() << std::endl;
-    }
+    // std::cout << i + static_cast<int> (f) << std::endl;
+
+    // std::cout << f << std::endl;
+    // std::cout << (float)i << std::endl;
+
+    // std::cout <<  f << std::endl;
 }
+
