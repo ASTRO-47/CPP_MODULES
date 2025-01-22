ffaaf45227b2fc931a0956e368e73f7ec3076296
aspro-47 <ezzaghba38@gmail.com>
2025-01-22 20:39:52 +0100

    done with ex00

--- Module-07/ex00/main.cpp
+++ Module-07/ex00/main.cpp
@@ -1,14 +1,21 @@
 #include "whatever.hpp"
 
+
 // RTTI : run time type informations
 
-int main()
-{  
-    int _42 = 42;
-    int second = 1337;
-    const int *test = &_42;
-    // *((int *)test) = 3;
-
-    std::cout << _42 << std::endl;
-    int * else_ = const_cast<int*>(test);
+int main( void ) 
+{
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
 }
--- Module-07/ex00/whatever.hpp
+++ Module-07/ex00/whatever.hpp
@@ -1,14 +1,40 @@
 #pragma once
 
 #include <iostream>
-template <class n>  void my_add(n n1, n n2)
-{
-    std::cout << "the adding result is: " << n1 + n2 << std::endl;
-}
+
+
 
 template <typename t> void swap(t &arg1, t &arg2)
 {
     t tmp = arg1;
     arg1 = arg2;
     arg2 = tmp;        
+}
+
+
+template <typename t_> t_ &min(t_ &arg1, t_ &arg2)
+{
+    if (arg1 >= arg2)
+        return arg2;
+    else
+        return arg1;
+}
+
+
+template <typename t_> t_ &max(t_ &arg1, t_ &arg2)
+{
+    if (arg1 <= arg2)
+        return arg2;
+    else
+        return arg1;
+}
+
+
+// template <typename he>
+
+template <typename data> 
+
+void print(data arge)
+{
+    std::cout << arge << std::endl;
 }
