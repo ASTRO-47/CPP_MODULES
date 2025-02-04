425c6130c62c84456852f445bfdd0830a57a2980
aspro-47 <ezzaghba38@gmail.com>
2025-02-04 16:04:14 +0100

    the const iterator added

--- Module-08/ex02/MutantStack.tpp
+++ Module-08/ex02/MutantStack.tpp
@@ -11,17 +11,20 @@ private:
     //
 public:
     typedef typename std::stack<T>::container_type::iterator iterator;
-    // typedef typename const std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
     MutantStack() : std::stack<T>() {};
     MutantStack(const MutantStack &other){
         *this = other;
     };
+
     MutantStack<T>& operator=(const MutantStack &other)
     {
         if(this != &other)
             std::stack<T>::operator=(other);
     }
 
+    const_iterator begin() const {puts("const"); return this->c.begin(); }
+    const_iterator end() const {puts("not const"); return this->c.end(); }
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end();}
     ~MutantStack(){};
--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -1,19 +1,26 @@
 #include "MutantStack.tpp"
-
+#include <vector>
 int main()
 {
-    MutantStack<int> imad;
+    // MutantStack<int> imad;
 
-    imad.push(3);
-
-    imad.push(6);
-    imad.push(34);
-    imad.push(56);
-
-    // imad
+    // imad.push(3);
+    // imad.push(6);
+    // imad.push(34);
+    // imad.push(56);
     
-    for (MutantStack<int>::iterator it = imad.begin();it !=imad.end(); it++)
-    {
-        std::cout << *it  << std::endl;
-    }
+    // for (MutantStack<int>::const_iterator it = imad.begin();it !=imad.end(); it++)
+    // {
+    //     std::cout << *it  << std::endl;
+    // }
+
+    std::vector<int> hello;
+    hello.push_back(-3);
+    hello.push_back(4);
+    hello.push_back(6);
+    hello.push_back(7);
+
+    // std::vector<int>::reverse_iterator  it = hello.rbegin();
+
+    // std::
 }
