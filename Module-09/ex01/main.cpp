e2642d90dd37b1dc2205bb6bccad48c2dd867eb6
aspro-47 <ezzaghba38@gmail.com>
2025-02-08 10:46:40 +0100

    adding the file parsing

---
+++ Module-09/ex01/main.cpp
@@ -0,0 +1,31 @@
+#include "easyfind.hpp"
+
+int main()
+{
+    std::vector<int> imad;
+
+    imad.push_back(1);
+    imad.push_back(2);
+    imad.push_back(3);
+    imad.push_back(4);
+
+    try
+    {
+        int  looking_for = easyfind(imad, 5);
+        std::cout << looking_for << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        int  looking_for = easyfind(imad, 4);
+        std::cout << "value found: " << looking_for << std::endl;
+
+    }
+    catch(std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
