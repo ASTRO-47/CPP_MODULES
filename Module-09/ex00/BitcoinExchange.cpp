5a54184d83cee8738250860e3f69400a1690294b
aspro-47 <ezzaghba38@gmail.com>
2025-02-10 11:21:28 +0100

    reading from the database file

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -21,27 +21,28 @@ void BitcoinExchange::open_files()
 
 void BitcoinExchange::load_data_base()
 {
-    std::ifstream file("data.csv");
+    std::ifstream data("data.csv");
     std::string line;
     std::string date;
     std::string str_value;
     double value;
-    if (!file.is_open())
+    if (!data.is_open())
         throw std::runtime_error("can't open database file");
-    // std::getline(file , line, '\n');
-    while (std::getline(file, line))
+    while (std::getline(data, line))
     {
         std::istringstream ss(line);
         std::getline(ss, date, ',');
-        value = std::strtod(str_value.c_str(), NULL);
+        // std::getline(ss, str_value, ',');
+        // std::cout << str_value << std::endl;
+        // value = std::strtod(str_value.c_str(), NULL); // need to know if the data intouchble
         std::cout.precision(15);
         ss >> value;
-        history[date] = value;
+        // history[date] = value;
+        std::cout << value << std::endl;
+        std::cout << date << std::endl;
+        break ;
     }
-    for (std::map<std::string , double>::iterator it = history.begin(); it != history.end();it++)
-    {
-        std::cout <<it->first << ","<<  it->second << std::endl;
-    }
+    data.close();
 }
 
 
