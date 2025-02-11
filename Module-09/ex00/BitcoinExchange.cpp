a0299e86669e16643f4535c7edfb0962a2857091
aspro-47 <ezzaghba38@gmail.com>
2025-02-11 11:48:54 +0100

    dataase parsed

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -22,28 +22,21 @@ void BitcoinExchange::open_files()
 void BitcoinExchange::load_data_base()
 {
     std::ifstream data("data.csv");
-    std::string line;
-    std::string date;
-    std::string str_value;
-    double value;
     if (!data.is_open())
         throw std::runtime_error("can't open database file");
+    std::string line;
+    std::string date;
+    double value;
     while (std::getline(data, line))
     {
         std::istringstream ss(line);
-        std::getline(ss, date, ',');
-        // std::getline(ss, str_value, ',');
-        // std::cout << str_value << std::endl;
-        // value = std::strtod(str_value.c_str(), NULL); // need to know if the data intouchble
+        if (std::getline(ss, date, ','))
+            return 
         std::cout.precision(15);
         ss >> value;
-        // history[date] = value;
-        std::cout << value << std::endl;
-        std::cout << date << std::endl;
-        break ;
+        history[date] = value;
     }
     data.close();
 }
 
-
-BitcoinExchange::~BitcoinExchange(){}
+BitcoinExchange::~BitcoinExchange()   {}
