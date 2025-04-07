69efe09c946546ec677b91b7988b79332c0c5dbb
aspro-47 <ezzaghba38@gmail.com>
2025-04-07 12:40:24 +0100

    parse and calculate

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -3,6 +3,42 @@
 RPN::~RPN() {}
 RPN::RPN() {}
 
-RPN::RPN(const std::string _exp) :_str(_exp) {
+RPN::RPN(const std::string _exp) :_str(_exp) {}
 
+bool RPN::its_Op(char c) const
+{
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+long long RPN::parse_calculate()
+{
+    std::cout << "stack size :" << _numbs.size() << std::endl;
+    for (size_t i = 0; i < _str.length(); i++)
+    {
+        if (std::isspace(_str[i]))
+            i++;
+        if (its_Op(_str[i]))
+        {
+            switch (_str[i])
+            {
+                case '+':
+                    puts("plus");
+                    break ;
+                case '-':
+                    puts("minus");
+                    break ;
+                case '*':
+                    puts("multi");
+                    break ;
+                case '/':
+                    puts("div");
+            }
+        }
+        else
+        {
+            std::cout << _str[i] << std::endl;
+        }
+        
+    }
+    return 0;
 }
--- Module-09/ex01/RPN.hpp
+++ Module-09/ex01/RPN.hpp
@@ -9,6 +9,7 @@ private:
     std::string _str;
     std::stack<int> _numbs;
     RPN();
+    bool    its_Op(char) const;
 public:
     RPN(const std::string);
     long long parse_calculate();
