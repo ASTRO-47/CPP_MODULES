8dc1f6ed9f36717cfe18eb9009a6213041c44d4e
aspro-47 <ezzaghba38@gmail.com>
2025-04-14 18:41:19 +0100

    optimizing the johnson's algo

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -44,6 +44,90 @@ bool    checker(std::string _m)
     return true;
 }
 
+void    PmergeMe::_jacob_gen()
+{
+    _jacob_seq.push_back(0);
+    _jacob_seq.push_back(1);
+    for (size_t i = 2; i < _vec.size(); i++)
+    {
+        _jacob_seq.push_back(_jacob_seq[i-1] + 2*_jacob_seq[i-2]);
+    }
+    for (size_t i = 0; i < _pairs.size(); i++)
+    {
+        _main_seq.push_back(_pairs[i].first);
+        _pend_seq.push_back(_pairs[i].second);
+    }
+
+    _main_seq.push_front(_pend_seq[0]);
+    size_t insert_count = 1; // we already inserted _pend_seq[0]
+    size_t jacob_idx = 1;
+    
+    while (insert_count < _pend_seq.size())
+    {
+        // Get the next Jacobsthal index within the range of pending sequence
+        size_t next_idx = _jacob_seq[jacob_idx];
+        while (next_idx < insert_count && next_idx < _pend_seq.size())
+        {
+            jacob_idx++;
+            next_idx = _jacob_seq[jacob_idx];
+        }
+        
+        // Process elements from current Jacobsthal number down to the previous inserted element
+        for (size_t i = std::min(next_idx, _pend_seq.size() - 1); i >= insert_count; i--)
+        {
+            if (i == 0) continue; // Skip the first element which is already inserted
+            
+            // Binary search to find insertion position in main sequence
+            size_t left = 0;
+            size_t right = insert_count + i; // The current sorted portion of main_seq
+            size_t element = _pend_seq[i];
+            
+            while (left < right)
+            {
+                size_t mid = left + (right - left) / 2;
+                if (_main_seq[mid] < element)
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+
+            // Insert at the found position
+            _main_seq.insert(_main_seq.begin() + left, element);
+            
+            if (i == 0) break; // To handle unsigned integer underflow
+        }
+        
+        insert_count = next_idx + 1;
+        jacob_idx++;
+        
+        if (insert_count >= _pend_seq.size())
+            break;
+    }
+    
+    // Handle any remaining elements
+    for (size_t i = insert_count; i < _pend_seq.size(); i++)
+    {
+        if (i == 0) continue; // Skip the first element which is already inserted
+        
+        // Binary search to find insertion position
+        size_t left = 0;
+        size_t right = _main_seq.size();
+        size_t element = _pend_seq[i];
+        
+        while (left < right)
+        {
+            size_t mid = left + (right - left) / 2;
+            if (_main_seq[mid] < element)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        
+        // Insert at the found position
+        _main_seq.insert(_main_seq.begin() + left, element);
+    }
+}
+
 void    PmergeMe::store_pairs()
 {
     size_t _s = _vec.size();
@@ -66,6 +150,13 @@ void    PmergeMe::store_pairs()
     // {
         // std::cout << _pairs[i].first << std::endl << _pairs[i].second << std::endl << std::endl; 
     // }
+    std::sort(_pairs.begin(), _pairs.end());
+    _jacob_gen();
+    // for (size_t i = 0; i < _main_seq.size(); i++)
+    // {
+        // std::cout << _pairs[i].first << std::endl << _pairs[i].second << std::endl << std::endl; 
+        // std::cout << _main_seq[i] << std::endl;
+    // }
 }
 
 void    PmergeMe::parse_sort(int ac, char *av[])
@@ -79,23 +170,31 @@ void    PmergeMe::parse_sort(int ac, char *av[])
         while (ss >> _geter)
         {
             if (!checker(_geter))
-                throw ("INVALID FORMAT");
+                throw std::runtime_error("INVALID FORMAT");
             int  _res = std::strtod(_geter.c_str(), &check);
             if (_res < 0 || check[0] != '\0')
-                throw ("INVALID FORMAT");
+                throw std::runtime_error("INVALID FORMAT");
             _vec.push_back(_res);
         }
     }
     std::cout << "Before:   ";
-    // for (size_t i = 0;i < _vec.size() ;i++)
-    // {
-    //     std::cout << _vec[i];
-    //     if (i != _vec.size() - 1)
-    //         std::cout << " ";
-    // }
-    // std::cout << std::endl ;
+    for (size_t i = 0;i < _vec.size() ;i++)
+    {
+        std::cout << _vec[i];
+        if (i != _vec.size() - 1)
+            std::cout << " ";
+    }
+    std::cout << std::endl ;
     store_pairs();
-    // std::cout << "After:   "; print all the container elements after sort
+    std::cout << "After:   ";
+    for (size_t i = 0;i < _main_seq.size() ;i++)
+    {
+        std::cout << _main_seq[i];
+        if (i != _main_seq.size() - 1)
+            std::cout << " ";
+    }
+    std::cout << std::endl;
+
 }
 
 
--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -15,6 +15,10 @@ private:
     std::vector<int>                    _vec;
     std::deque<int>                     _deq;
     std::vector<std::pair<int, int> >    _pairs;
+    std::vector<int>                    _jacob_seq;
+    std::deque<size_t>                    _main_seq;
+    std::deque<size_t>                    _pend_seq;
+    void                                _jacob_gen();
 public:
     PmergeMe(int, char *[]);
     PmergeMe(const PmergeMe &);
